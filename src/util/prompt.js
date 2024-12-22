5700a70e87c38ec60d8a0e28500e8b3b59712e9a
由比ゅ♂ <sa192@momoi.uk>
2024-12-22 03:09:45 +0000

    update code

--- src/util/prompt.js
+++ src/util/prompt.js
@@ -1,5 +1,5 @@
 export function prompt(question) {
-    if (typeof question != "string") throw new Error("The question is invalid");
+    if (typeof question != "string") throw new Error("The question is invalid.");
     return new Promise(resolve => {
         process.stdout.write(question);
         process.stdin.once("data", (data) => {
--- src/util/sleep.js
+++ src/util/sleep.js
@@ -1,4 +1,4 @@
 export async function sleep(ms) {
-    if (isNaN(ms)) throw new Error("The ms is invalid");
+    if (isNaN(ms)) throw new Error("The ms is invalid.");
     return new Promise(resolve => setTimeout(resolve, ms));
 }
